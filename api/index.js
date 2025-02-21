5209236aade24db1ec1bd3e2eea5275bbfa46a89
Prashanna <prashannaraghu@gmail.com>
2025-02-21 18:47:14 +0530

    REVERTING TO test api route

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -22,20 +21,3 @@ app.listen(3000, () =>{
 });
 
 app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
-
-// process.on("SIGINT", async () => {
-//    await mongoose.connection.close();
-//    console.log("MongoDB connection closed due to app termination.");
-//    process.exit(0);
-// });
-
-app.use((err, req, res, next) => {
-   const statusCode = err.statusCode || 500;
-   const message = err.message || 'Internal Server Error';
-   res.status(statusCode).json({
-      success: false,
-      statusCode,
-      message,
-   });
-});
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,49 +1,7 @@
-import { Link } from 'react-router-dom';
+import React from 'react'
 
 export default function SignUp() {
-  return (
-    <div className='min-h-screen mt-20'>
-      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
-        {/* left */}
-        <div className='flex-1'>
-          <Link to='/' className='font-bold dark:text-white text-4xl'>
-            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-              Sahand's
-            </span>
-            Blog
-          </Link>
-          <p className='text-sm mt-5'>
-            This is a demo project. You can sign up with your email and password
-            or with Google.
-          </p>
-        </div>
-        {/* right */}
-        <div className='flex-1'>
-          <form className='flex flex-col gap-4'>
-            <div>
-              <label className="block text-sm font-medium text-gray-700">Your username</label>
-              <input type='text' placeholder='Username' id='username' className="mt-1 block w-full p-2 border border-gray-300 rounded-md" />
-            </div>
-            <div>
-              <label className="block text-sm font-medium text-gray-700">Your email</label>
-              <input type='text' placeholder='name@company.com' id='email' className="mt-1 block w-full p-2 border border-gray-300 rounded-md" />
-            </div>
-            <div>
-              <label className="block text-sm font-medium text-gray-700">Your password</label>
-              <input type='password' placeholder='Password' id='password' className="mt-1 block w-full p-2 border border-gray-300 rounded-md" />
-            </div>
-            <button type='submit' className='bg-gradient-to-r from-purple-500 to-pink-500 text-white p-2 rounded-md'>
-              Sign Up
-            </button>
-          </form>
-          <div className="flex gap-2 text-sm mt-5">
-            <span>Have an account?</span>
-            <Link to='/sign-in' className='text-blue-500'>
-              Sign In
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+   return (
+      <div>SignUp</div>
+   )
 }
