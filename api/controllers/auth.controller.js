c55e49e507e427f11735732215d92cb9ecc0e8c4
Prashanna <prashannaraghu@gmail.com>
2025-02-21 20:17:48 +0530

    REVERTING TO create signup api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,11 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res, next) => {
+export const signup = async (req, res) => {
    const { username, email, password } = req.body;
 
    if(!username || !email || !password || username === '' || email === '' || password === ''){
-      next(errorHandler(400, 'All fields are required'));
+      return res.status(400).json({ message: 'All fields are required'});
    }
 
    const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -21,6 +20,6 @@ export const signup = async (req, res, next) => {
       await newUser.save();
       res.json('Signup successful');
    } catch (error) {
-      next(error);
+      res.status(500).json({ message: error.message});
    }
 };
